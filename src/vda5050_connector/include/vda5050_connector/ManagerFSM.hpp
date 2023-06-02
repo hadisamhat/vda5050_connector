5f1cd272a0ecc9f8422694feaa75907b2cf2abbf
hadi <hadisamhat@hotmail.com>
2023-06-02 12:46:06 +0000

    removed publish comments

--- src/vda5050_connector/include/vda5050_connector/ManagerFSM.hpp
+++ src/vda5050_connector/include/vda5050_connector/ManagerFSM.hpp
@@ -414,7 +414,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
               tx_state_.msg.header.manufacturer = config_.manufacturer;
               tx_state_.msg.header.serialNumber = config_.serial_number;
               auto j = tx_state_.msg.to_json();
-              // logger_->logInfo("state timeout elapsed, publishing data" + j.dump());
+              logger_->logInfo("state timeout elapsed, publishing data" + j.dump());
               //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
               //   j.dump().length()); connection_->Publish(tx_visualization_.topic_name.c_str(),
               //   AWS_MQTT_QOS_AT_LEAST_ONCE,
@@ -430,7 +430,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
                   tx_connection_.msg.header.manufacturer = config_.manufacturer;
                   tx_connection_.msg.header.serialNumber = config_.serial_number;
                   auto j = tx_connection_.msg.to_json();
-                  // logger_->logInfo("Connection timeout elapsed, publishing data" + j.dump());
+                  logger_->logInfo("Connection timeout elapsed, publishing data" + j.dump());
                   //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
                   //   j.dump().length());
                   //   connection_->Publish(tx_visualization_.topic_name.c_str(),
@@ -447,7 +447,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
                   tx_fact_sheet_.msg.header.manufacturer = config_.manufacturer;
                   tx_fact_sheet_.msg.header.serialNumber = config_.serial_number;
                   auto j = tx_fact_sheet_.msg.to_json();
-                  // logger_->logInfo("Fact Sheet timeout elapsed, publishing data" + j.dump());
+                  logger_->logInfo("Fact Sheet timeout elapsed, publishing data" + j.dump());
                   //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
                   //   j.dump().length());
                   //   connection_->Publish(tx_visualization_.topic_name.c_str(),
@@ -464,8 +464,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
                   tx_visualization_.msg.header.manufacturer = config_.manufacturer;
                   tx_visualization_.msg.header.serialNumber = config_.serial_number;
                   auto j = tx_visualization_.msg.to_json();
-                  // logger_->logInfo("Visualization timeout elapsed, publishing data" +
-                  // j.dump());
+                  logger_->logInfo("Visualization timeout elapsed, publishing data" + j.dump());
                   //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
                   //   j.dump().length());
                   //   connection_->Publish(tx_visualization_.topic_name.c_str(),

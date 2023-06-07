b7704cf38a7feb546731a30cebbe736cbcae24a9
hadi <hadisamhat@hotmail.com>
2023-06-07 15:57:33 +0200

    updated logs

--- src/vda5050_connector/include/vda5050_connector/ManagerFSM.hpp
+++ src/vda5050_connector/include/vda5050_connector/ManagerFSM.hpp
@@ -156,7 +156,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
       tx_state_.msg.header.manufacturer = config_.manufacturer;
       tx_state_.msg.header.serialNumber = config_.serial_number;
       auto j = tx_state_.msg.to_json();
-      // logger_->logInfo("publishing update state msg" + j.dump());
+      logger_->logInfo("updated state msg" + j.dump());
       //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
       //   j.dump().length()); connection_->Publish(tx_visualization_.topic_name.c_str(),
       //   AWS_MQTT_QOS_AT_LEAST_ONCE,
@@ -173,7 +173,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
       tx_visualization_.msg.header.manufacturer = config_.manufacturer;
       tx_visualization_.msg.header.serialNumber = config_.serial_number;
       auto j = tx_visualization_.msg.to_json();
-      // logger_->logInfo("publishing update visualization msg" + j.dump());
+      logger_->logInfo("updated visualization msg" + j.dump());
       //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
       //   j.dump().length()); connection_->Publish(tx_visualization_.topic_name.c_str(),
       //   AWS_MQTT_QOS_AT_LEAST_ONCE,
@@ -190,7 +190,7 @@ class ManagerFSM : public interface::BaseManagerInterface<OrderMsg, InstantActio
       tx_connection_.msg.header.manufacturer = config_.manufacturer;
       tx_connection_.msg.header.serialNumber = config_.serial_number;
       auto j = tx_connection_.msg.to_json();
-      logger_->logInfo("publishing update connection msg" + j.dump());
+      logger_->logInfo("updated connection msg" + j.dump());
       //   ByteBuf payload = ByteBufFromArray((const uint8_t*)j.dump().data(),
       //   j.dump().length()); connection_->Publish(tx_visualization_.topic_name.c_str(),
       //   AWS_MQTT_QOS_AT_LEAST_ONCE,
